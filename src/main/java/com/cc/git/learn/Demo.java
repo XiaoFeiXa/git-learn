511f8d499d90d016fbb8463b6f8a207144a75984
chenchao <ccofqlxj@163.com>
2019-12-18 16:50:18 +0800

    03

--- src/main/java/com/cc/git/learn/Demo.java
+++ src/main/java/com/cc/git/learn/Demo.java
@@ -5,5 +5,6 @@ package com.cc.git.learn;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("开始GIT学习...");
+        System.out.println("log...");
     }
 }
