78a77f9275244ce106b12887a5e2cf426804f724
XiaoFeiXa <54489557+XiaoFeiXa@users.noreply.github.com>
2019-12-18 16:48:34 +0800

    Update Demo.java

--- src/main/java/com/cc/git/learn/Demo.java
+++ src/main/java/com/cc/git/learn/Demo.java
@@ -5,7 +5,7 @@ package com.cc.git.learn;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("开始GIT学习...");
-        System.out.println("更改1......");
+        System.out.println("1......");
         System.out.println("更2......");
     }
 
